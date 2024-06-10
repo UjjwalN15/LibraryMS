fb126591d3e8f3de2c5b065508ac5d7dd7eb0e9f
Ujjwal Neupane <itsmeujjwal725@gmail.com>
2024-06-10 18:42:05 +0545

    API tested in Postman

--- base/models.py
+++ base/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=300)
+    name = models.CharField(max_length=300, unique=True)
     def __str__(self):
         return self.name
     
 class Author(models.Model):
     name = models.CharField(max_length=300)
-    number = models.CharField(max_length=300,unique=True,null=False)
-    email = models.EmailField(unique=True, null=False)
-    address = models.CharField(max_length=300)
+    number = models.CharField(max_length=300,unique=True,null=True)
+    email = models.EmailField(unique=True, null=True)
+    address = models.CharField(max_length=300, null=True)
     def __str__(self):
         return self.name
     
@@ -20,7 +20,7 @@ class Book(models.Model):
     description = models.TextField()
     price = models.FloatField()
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
     published_date = models.DateField()
     stock = models.IntegerField()
     def __str__(self):
--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('category/',CategoryApiView.as_view({'get':'list','post':'create'}),name='category'),
-    path('category/<id:pk>/',CategoryApiView.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name='category'),
+    path('category/<int:pk>/',CategoryApiView.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name='category'),
     path('author/',AuthorApiView.as_view({'get':'list','post':'create'}),name='author'),
-    path('author/<id:pk>/',AuthorApiView.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name='author'),
+    path('author/<int:pk>/',AuthorApiView.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name='author'),
     path('book/',BookApiView.as_view({'get':'list','post':'create'}),name='book'),
-    path('book/<id:pk>/',BookApiView.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name='book'),
+    path('book/<int:pk>/',BookApiView.as_view({'get':'retrieve','put':'update','delete':'destroy'}),name='book'),
 ]
