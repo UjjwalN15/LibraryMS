00fd38f2ea70ae7cdd640edda6effa6812620073
Ujjwal Neupane <itsmeujjwal725@gmail.com>
2024-06-10 17:52:04 +0545

    Views created

--- base/views.py
+++ base/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .serializers import *
 # Create your views here.
+
+class CategoryApiView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class AuthorApiView(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    
+class BookApiView(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
